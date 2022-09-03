95d305513a04d7a7f3644a79436ffe9ca5f6c1c3
Aeull <110038479+Aeull@users.noreply.github.com>
2022-09-03 12:28:38 +0800

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -1,92 +1,43 @@
-const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys-md')
-const { servers, yta, ytv } = require('../lib/y2mate')
-let fs = require('fs')
-let yts = require('yt-search')
-let fetch = require('node-fetch')
-let handler = async (m, { conn, command, text, usedPrefix }) => {
-  if (!text) throw `uhm.. cari apa?\n\ncontoh:\n${usedPrefix + command} california`
-  let chat = global.db.data.chats[m.chat]
-  let results = await yts(text)
-  let vid = results.all.find(video => video.seconds < 3600)
-  if (!vid) throw 'Konten Tidak ditemukan'
-  let isVideo = /2$/.test(command)
-  let yt = false
-  let yt2 = false
-  let usedServer = servers[0]
-  for (let i in servers) {
-    let server = servers[i]
-    try {
-      yt = await yta(vid.url, server)
-      yt2 = await ytv(vid.url, server)
-      usedServer = server
-      break
-    } catch (e) {
-      m.reply(`Server ${server} error!${servers.length >= i + 1 ? '' : '\nmencoba server lain...'}`)
-    }
-  }
-  if (yt === false) throw 'semua server gagal'
-  if (yt2 === false) throw 'semua server gagal'
-  let { dl_link, thumb, title, filesize, filesizeF } = yt
-let anu =  `
-*Judul:* ${title}
-*Ukuran File Audio:* ${filesizeF}
-*Ukuran File Video:* ${yt2.filesizeF}
-*Server y2mate:* ${usedServer}
-*link sumber:* 
-${vid.url}
-
-`
-     const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
-     templateMessage: {
-         hydratedTemplate: {
-           hydratedContentText: anu,
-           locationMessage: { 
-           jpegThumbnail: await (await fetch(thumb)).buffer() }, 
-           hydratedFooterText: `jika video tidak sesuai
-silahkan ketik .yta link YouTube untuk mengunduh audio
-dan ketik .ytv link YouTube Untuk Mengunduh Video`,
-           hydratedButtons: [{
-             urlButton: {
-               displayText: 'MY GITHUB',
-               url: github
-             }
-
-           },
-               {
-             quickReplyButton: {
-               displayText: 'video 360p',
-               id: `.ytmp4 ${vid.url}`,
-             }
-
-            },
-               {
-             quickReplyButton: {
-               displayText: 'video 720p',
-               id: `.ytv720 ${vid.url}`,
-             }
-
-            },
-               {
-             quickReplyButton: {
-               displayText: 'Audio',
-               id: `.ytmp3 ${vid.url}`,
-             }
-
-           }]
-         }
-       }
-     }), { userJid: m.sender, quoted: m });
-    //conn.reply(m.chat, text.trim(), m)
-    return await conn.relayMessage(
-         m.chat,
-         template.message,
-         { messageId: template.key.id }
-     )
-}
-handler.help = ['play'].map(v => v + ' <pencarian>')
-handler.tags = ['downloader']
-handler.command = /^(p|play)$/i
-
-handler.exp = 0
-
-module.exports = handler
+let yts = require('yt-search') 
+ let fetch = require('node-fetch') 
+ const { servers, yta, ytv } = require('../lib/y2mate') 
+ let handler = async (m, { conn, command, usedPrefix, text, isPrems, isOwner }) => { 
+   if (!text) throw `uhm.. cari apa?\n\ncontoh:\n${usedPrefix + command} i see your monster` 
+   let chat = global.db.data.chats[m.chat] 
+   let results = await yts(text) 
+   let vid = results.all.find(video => video.seconds < 3600) 
+   if (!vid) throw 'Video/Audio Tidak ditemukan' 
+   let isVideo = /2$/.test(command) 
+   let yt = false 
+   let usedServer = servers[0] 
+   for (let i in servers) { 
+     let server = servers[i] 
+     try { 
+       yt = await yta(vid.url, server) 
+       yt2 = await ytv(vid.url, server) 
+       usedServer = server 
+       break 
+     } catch (e) { 
+       m.reply(`Server ${server} error!${servers.length >= i + 1 ? '' : '\nmencoba server lain...'}`) 
+     } 
+   } 
+   if (yt === false) throw 'semua server gagal' 
+   if (yt2 === false) throw 'semua server gagal' 
+   let { dl_link, thumb, title, filesize, filesizeF } = yt 
+   await conn.send3ButtonLoc(m.chat, await conn.resize(thumb, 280, 210), ` 
+ *💌 Judul:* ${title}
+*🎶 Audio:* ${filesizeF}
+*🎥 Video:* ${yt2.filesizeF}
+*💻 Server y2mate:* ${usedServer}
+*⛓️ Link Video:* ${vid.url}
+  `.trim(), wm, `🎙️ Audio`, `.yta ${vid.url}`, `🎥 Video`, `.yt ${vid.url}`, '🔎 YouTube Search', `.yts ${title}`, m) 
+ } 
+ handler.help = ['play'].map(v => v + ' <pencarian>') 
+ handler.tags = ['downloader'] 
+ handler.command = /^(dj|musik|song|lagu|p(lay)?)$/i 
+  
+ handler.exp = 3 
+ handler.limit = false 
+ handler.register = false 
+  
+ module.exports = handler
