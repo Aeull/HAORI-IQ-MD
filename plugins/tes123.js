1a8cb06ff631d52300f77affe0d4ee55d32dc59b
Aeull <110038479+Aeull@users.noreply.github.com>
2022-09-03 09:54:41 +0800

    Update tes123.js

--- plugins/tes123.js
+++ plugins/tes123.js
@@ -3,7 +3,7 @@ let fetch = require('node-fetch')
 let handler  = async (m, { conn, usedPrefix: _p }) => {
 m.reply('Bot On')
 }
-handler.customPrefix = /^(tes|tess|test)$/i
+handler.customPrefix = /^(tes|tess|test|bot)$/i
 handler.command = new RegExp
 
 module.exports = handler
